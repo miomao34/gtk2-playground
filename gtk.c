0c02d0fc5d7f894209cd569cddaee3fd7a252654
miomao34 <flex384@gmail.com>
2019-06-22 23:37:01 +0300

    added threading led display

--- gtk.c
+++ gtk.c
@@ -1,11 +1,63 @@
 #include <gtk/gtk.h>
+#include <pthread.h> 
+#include "led-matrix-connector/led-matrix-connector/led-matrix-connector.h"
+
+void myExit()
+{
+	LedClear();
+	printf("\n");
+	return;
+}
+
+void end_program (GtkWidget *wid, gpointer ptr)
+{
+	myExit();
+	gtk_main_quit ();
+}
+
+void *display_thread(void *vargp)
+{
+	LedRenderText("TEST MSG", 300);
+}
+
+void display (GtkWidget *wid, gpointer ptr)
+{
+	pthread_t thread_id;
+	pthread_create(&thread_id, NULL, display_thread, NULL);
+	pthread_detach(thread_id);
+}
 
 void main (int argc, char *argv[])
 {
-	gtk_init (&argc, &argv);
-	GtkWidget *win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	LedConfig Config = {
+		.orientation = 0,
+		.spiDevice = 1,
+		.primaryColor = 'b',
+		.delayTime = 1,
+		.exitFunction = myExit
+	};
+	LedInit(Config);
+	
+	gtk_init(&argc, &argv);
+	GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(win, "delete_event", G_CALLBACK (end_program), NULL);
+	
+	GtkWidget *box = gtk_vbox_new(FALSE, 0);
+	
 	GtkWidget *btn = gtk_button_new_with_label ("Close window");
-	gtk_container_add (GTK_CONTAINER (win), btn);
-	gtk_widget_show_all (win);
-	gtk_main ();
+	g_signal_connect (btn, "clicked", G_CALLBACK (end_program), NULL);
+	gtk_box_pack_start (GTK_BOX (box), btn, TRUE, TRUE, 0);
+	
+	GtkWidget *lbl = gtk_label_new("test");
+	gtk_box_pack_start (GTK_BOX (box), lbl, FALSE, TRUE, 0);
+	
+	GtkWidget *btn2 = gtk_button_new_with_label ("SURPRISE"); 
+	g_signal_connect (btn2, "clicked", G_CALLBACK (display), NULL); 
+	gtk_box_pack_start (GTK_BOX (box), btn2, TRUE, TRUE, 0);
+	
+	gtk_container_add (GTK_CONTAINER (win), box); 
+	gtk_widget_show_all(win);
+	gtk_main();
+	
+	LedFinalise();
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc `pkg-config --cflags --libs gtk+-2.0` gtk.c -o gtk.out
+	gcc `pkg-config --cflags --libs gtk+-2.0` led-matrix-connector/led-matrix-connector/led-matrix-connector.c gtk.c -o gtk.out -lwiringPi -lpthread
